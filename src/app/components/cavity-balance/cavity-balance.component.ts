7101cad2988e49911c1a99f113a956079ae3ede6
Maximiliano Ponce Marquez <maxponce.marquez@gmail.com>
2021-01-10 13:49:26 -0700

    Started Cavity Balance

--- src/app/components/cavity-balance/cavity-balance.component.ts
+++ src/app/components/cavity-balance/cavity-balance.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cavity-balance',
   templateUrl: './cavity-balance.component.html',
-  styleUrls: ['./cavity-balance.component.css']
+  styleUrls: ['./cavity-balance.component.css'],
 })
 export class CavityBalanceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  calculate() {}
 }
