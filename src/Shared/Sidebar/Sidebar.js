93515f29d3fee03488057df2a748a6126f72f1fa
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2021-05-04 07:01:21 -0700

    Chevron icon imported in sidebar

--- src/Shared/Sidebar/Sidebar.js
+++ src/Shared/Sidebar/Sidebar.js
@@ -1,9 +1,13 @@
 import "./Sidebar.scss";
+import chevronIcon from "../../Assets/SVG/bxs-chevron-left-circle.svg";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <button className="active">Home</button>
+      <button className="sidebar__toggler">
+        <img src={chevronIcon} />
+      </button>
+      <button className="active sidebar__home">Home</button>
       <button>Viscosity Curve</button>
       <button>Cavity Balance</button>
       <button>Pressure Drop</button>
