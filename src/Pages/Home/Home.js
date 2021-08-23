0a4b9871593b8670dd23c6b5b4a54cedb384033d
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2021-08-23 06:22:58 -0700

    Sidebar implemented, home page finished

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
-import './Home.scss';
-import moldingMachineIcon from '../../Assets/Images/MoldingMachine.svg';
+import "./Home.scss";
+import moldingMachineIcon from "../../Assets/Images/MoldingMachine.svg";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <h2 className='home--title'>Welcome</h2>
-      <img src={moldingMachineIcon} alt='Molding Machine' className='home--img' />
+    <div className="home">
+      <h2 className="home--title">Welcome to Scientific Molding Tools</h2>
+      <img
+        src={moldingMachineIcon}
+        alt="Molding Machine"
+        className="home--img"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
