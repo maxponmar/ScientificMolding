cf35290bb60b38274060dedfe1dd4ce0684496b6
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2021-08-23 06:27:14 -0700

    Viscosity curve folder and files created, home message changed to Welcome

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import moldingMachineIcon from "../../Assets/Images/MoldingMachine.svg";
 const Home = () => {
   return (
     <div className="home">
-      <h2 className="home--title">Welcome to Scientific Molding Tools</h2>
+      <h2 className="home--title">Welcome</h2>
       <img
         src={moldingMachineIcon}
         alt="Molding Machine"
