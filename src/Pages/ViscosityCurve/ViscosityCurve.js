0f3496bce924195e97e37c1cf469bf213feed93e
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2021-08-25 05:40:52 -0700

    Creating form for viscosity curve

--- src/Pages/ViscosityCurve/ViscosityCurve.js
+++ src/Pages/ViscosityCurve/ViscosityCurve.js
@@ -1,7 +1,30 @@
 import "./ViscosityCurve.scss";
+import { useState } from "react";
 
 function ViscosityCurve() {
-  return <div>Hola</div>;
+  const [intensificationRatio, setIntensificationRatio] = useState(0);
+
+  const handleIntensificationRatioChange = (event) => {
+    setIntensificationRatio(event.target.value);
+  };
+
+  return (
+    <div className="Viscositycurve">
+      <h2 className="study-title">Viscosity Curve</h2>
+      <div>
+        <form>
+          <div>
+            <label>Intensification Ratio</label>
+            <input
+              type="text"
+              value={intensificationRatio}
+              onChange={handleIntensificationRatioChange}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ViscosityCurve;
